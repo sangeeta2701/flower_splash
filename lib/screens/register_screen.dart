535727aee5271c85d8cc4fa0dfba35a97dc9bfa8
DELL <anjugupta7060@gmail.com>
2022-08-13 10:42:08 +0530

    Flower Bouquet Shopping

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -24,133 +24,112 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 30.0),
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Bouquet Zone",
-                  style: TextStyle(
-                      fontSize: 35,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w900),
+          padding: const EdgeInsets.only(top: 150.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Bouquet Zone",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "Register",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: TextInput(
+                  lableText: "Username",
+                  icon: Icons.person,
+                  controller: _userNameController,
                 ),
-                const SizedBox(
-                  height: 15,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: TextInput(
+                  lableText: "Email",
+                  icon: Icons.email,
+                  controller: _emailController,
                 ),
-                const Text(
-                  "Register",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextInput(
+                      controller: _passwordController,
+                      lableText: "Password",
+                      icon: Icons.lock)),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade100,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(5),
+                  ),
                 ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Stack(
-                  children: [
-                    const CircleAvatar(
-                      radius: 64,
-                      backgroundImage: AssetImage("assets/dp.png"),
-                      backgroundColor: Colors.black,
-                    ),
-                    Positioned(
-                      bottom: -10,
-                      left: 80,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.add_a_photo),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.only(left: 20, right: 20),
-                  child: TextInput(
-                    lableText: "Username",
-                    icon: Icons.person,
-                    controller: _userNameController,
                   ),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.only(left: 20, right: 20),
-                  child: TextInput(
-                    lableText: "Email",
-                    icon: Icons.email,
-                    controller: _emailController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account? ",
+                    style: TextStyle(fontSize: 20),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: const EdgeInsets.only(left: 20, right: 20),
-                    child: TextInput(
-                        controller: _passwordController,
-                        lableText: "Password",
-                        icon: Icons.lock)),
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width - 40,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.shade100,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(5),
-                    ),
-                  ),
-                  child: InkWell(
+                  InkWell(
                     onTap: () {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
                               builder: (context) => LoginScreen()));
                     },
-                    child: const Center(
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 20, color: Colors.blue),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Already have an account? ",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    InkWell(
-                      onTap: () {},
-                      child: Text(
-                        "Login",
-                        style: TextStyle(fontSize: 20, color: Colors.blue),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:splash_screen/screens/register_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,22 +13,35 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 6), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => RegisterScreen()));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/logo1.png"),
-            Text(
-              "Bouquet Zone",
-              style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset("assets/92886-flowering-plant.json"),
+          Text(
+            "Bouquet Zone",
+            style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue.shade200),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+        ],
       ),
     );
   }
